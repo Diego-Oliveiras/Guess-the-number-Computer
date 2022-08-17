44f4995402ff7e465c35f364a7c247e297353056
opschat <ops@chat2desk.com.br>
2022-08-16 22:40:24 -0300

    Implementação de menu e score

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import random
+score_player = 0
+score_computer = 0
+life = 3
+fim = False
 
 
-def print_life(life):
+def reset_var():
+    global life
+    life = 3
+    return life
+
+
+def print_life_player(life):
     msg = f'You have {life} lives'
     return print(msg)
 
 
+def print_life_computer(life):
+    msg = f'Computer have {life} lives'
+    return print(msg)
+
+
 def guess(x):
-    life = 2
+    global score_player
+    global life
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number and life > 0:
@@ -16,39 +32,74 @@ def guess(x):
 
         if guess < random_number:
             print('Sorry , guess again.Too low.')
-            print_life(life)
+            print_life_player(life)
         elif guess > random_number:
             print('Sorry , guess again.Too high.')
-            print_life(life)
+            print_life_player(life)
         elif guess == random_number:
             print(
                 f'Yay , congrats. You have guessed the number {random_number}')
+            score_player = score_player + 1
+            return score_player
+
         if life == 0:
             print("Gamer Over!")
 
 
 def computer_guess(x):
+    global score_computer
+    global life
     low = 1
     high = x
     feedback = ''
-    while feedback != 'c':
+    while feedback != 'c' and life > 0:
         if low != high:
             guess = random.randint(low, high)
         else:
             guess = low
         feedback = input(
-            f'Is {guess} too hight(H) , too low (L) or too Correct (c)').lower()
+            f'Is {guess} too hight(H) , too low (L) or too Correct (c): ').lower()
+
         if feedback == 'h':
             high = guess - 1
         elif feedback == 'l':
             low = guess + 1
+
         elif feedback == 'c':
             print(
                 f'Congrats!!The Computer guessed your number: {guess},Correctly ')
+            score_computer += 1
+            return score_computer
         else:
             print('Sorry,Value invalid feedback')
+        if life == 0:
+            print("Gamer Over!")
+
+        life = life - 1
+        print_life_computer(life)
 
 
-numbers = int(input('Choose the maximum amount of numbers you want to play: '))
-# guess(numbers)
-computer_guess(numbers)
+while fim == False:
+    print()
+    print('Score')
+    print(f'PLayer:{score_player} , Computer:{score_computer}')
+    print()
+    print('who will guess?')
+    print('1-You')
+    print('2-Computer')
+    print('3-Exit')
+    resp = int(input())
+
+    if resp == 1:
+        numbers = int(
+            input('Choose the maximum amount of numbers you want to play: '))
+        guess(numbers)
+        reset_var()
+    elif resp == 2:
+        numbers = int(
+            input('Choose the maximum amount of numbers you want to play: '))
+        computer_guess(numbers)
+        reset_var()
+    elif resp == 3:
+        print('Bye!')
+        fim = True
